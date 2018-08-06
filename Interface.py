f3788fb23e688951ff0c14301e89697cda73b148
Ireneusz Larysz <ireneuszlarysz@gmail.com>
2018-08-06 12:20:40 +0200

    Updated interface, before adding subplots with ind

--- Interface.py
+++ Interface.py
@@ -30,6 +30,134 @@ major_pairs = ("AUD_USD", "EUR_CHF", "EUR_USD", "GBP_JPY", "GBP_USD", "NZD_USD",
 user_pair_choice = 0
 user_candles_count_choice = 150
 user_granularity_choice = 'H1'
+chart_indicator = ['name', 0]
+bottom_indicator = ['name', 0, 0, 0]
+animation_status = True
+
+
+def set_indicators(chart=None, bottom=None):
+
+    if chart:
+        if chart == "sma":
+            indicator_setter = tk.Tk()
+            indicator_setter.wm_title("Configure")
+            label1 = ttk.Label(indicator_setter, text="Set the period for moving average")
+            label1.pack(padx=10, pady=5)
+            entry1 = ttk.Entry(indicator_setter)
+            entry1.insert(0, 14)
+            entry1.pack(padx=10, fill='x')
+            entry1.focus_set()
+
+            def callback():
+                global chart_indicator
+                chart_indicator[0] = 'sma'
+                chart_indicator[1] = entry1.get()
+                print("Indicator {} set with period of {}".format(chart_indicator[0], chart_indicator[1]))
+
+                indicator_setter.destroy()
+
+            button1 = ttk.Button(indicator_setter, text="Ok", width=10, command=callback)
+            button1.pack(padx=10, pady=5, fill='x')
+
+            indicator_setter.mainloop()
+
+        elif chart == 'ema':
+            indicator_setter = tk.Tk()
+            indicator_setter.wm_title("Configure")
+            label1 = ttk.Label(indicator_setter, text="Set the period for moving average")
+            label1.pack(padx=10, pady=5)
+            entry1 = ttk.Entry(indicator_setter)
+            entry1.insert(0, 14)
+            entry1.pack(padx=10, fill='x')
+            entry1.focus_set()
+
+            def callback():
+                global chart_indicator
+                chart_indicator[0] = 'ema'
+                chart_indicator[1] = entry1.get()
+                print("Indicator {} set with period of {}".format(chart_indicator[0], chart_indicator[1]))
+
+                indicator_setter.destroy()
+
+            button1 = ttk.Button(indicator_setter, text="Ok", width=10, command=callback)
+            button1.pack(padx=10, pady=5, fill='x')
+
+            indicator_setter.mainloop()
+
+    if bottom:
+        if bottom == 'rsi':
+            # Show the window to let user choose RSI period
+            rsi_setter = tk.Tk()
+            rsi_setter.wm_title("Configure RSI")
+
+            label1 = tk.Label(rsi_setter, text="Set the period for rsi", font=NORM_FONT)
+            label1.pack(side='top', pady=10, padx=10)
+            entry1 = tk.Entry(rsi_setter)
+            entry1.insert(0, 7)
+            entry1.pack()
+            entry1.focus_set()
+
+            def callback():
+                global bottom_indicator
+                bottom_indicator[0] = bottom
+                bottom_indicator[1] = entry1.get()
+                print(f"Indicator set to {bottom_indicator[0]} with period of {bottom_indicator[1]}")
+                rsi_setter.destroy()
+
+            button1 = ttk.Button(rsi_setter, text="Ok", width=10, command=callback)
+            button1.pack()
+            rsi_setter.mainloop()
+
+        elif bottom == 'macd':
+            macd_setter = tk.Tk()
+            macd_setter.wm_title("Configure MACD")
+
+            label0 = ttk.Label(macd_setter, text="Set the periods for MACD")
+            label0.grid(row=0, column=0, columnspan=2)
+
+            label1 = ttk.Label(macd_setter, text='Faster MA')
+            label1.grid(row=1, column=0, pady=5, padx=5)
+            entry1 = ttk.Entry(macd_setter)
+            entry1.insert(0, 12)
+            entry1.grid(row=1, column=1, pady=5, padx=5)
+            entry1.focus_set()
+
+            label2 = ttk.Label(macd_setter, text='Slower MA')
+            label2.grid(row=2, column=0, pady=5, padx=5)
+            entry2 = ttk.Entry(macd_setter)
+            entry2.insert(0, 26)
+            entry2.grid(row=2, column=1, pady=5, padx=5)
+
+            label3 = ttk.Label(macd_setter, text='Signal line')
+            label3.grid(row=3, column=0, pady=5, padx=5)
+            entry3 = ttk.Entry(macd_setter)
+            entry3.insert(0, 9)
+            entry3.grid(row=3, column=1, pady=5, padx=5)
+
+            def callback():
+                global bottom_indicator
+                bottom_indicator[0] = bottom
+                bottom_indicator[1] = entry1.get()
+                bottom_indicator[2] = entry2.get()
+                bottom_indicator[3] = entry3.get()
+
+                print("Indicator set to {indicator} with following parameters: {first_param}, {second_param}, "
+                      "{third_param}".format(indicator=bottom_indicator[0], first_param=bottom_indicator[1],
+                                             second_param=bottom_indicator[2], third_param=bottom_indicator[3]))
+
+                macd_setter.destroy()
+
+            button1 = ttk.Button(macd_setter ,text="Ok", command=callback)
+            button1.grid(row=5, column=0, pady=5, columnspan=2)
+
+            macd_setter.mainloop()
+
+def animation_changer(status):
+    global animation_status
+    if status == 'on':
+        animation_status = True
+    elif status == 'off':
+        animation_status = False
 
 
 def tf_changer(tf):
@@ -80,23 +208,35 @@ def popupmsg(msg, title="Error"):
 
     popup.mainloop()
 
+def create_bottom_indicator():
+    if bottom_indicator == 'macd':
+        pass
+    elif bottom_indicator == 'rsi':
+        pass
+
+
+def create_chart_indicator():
+    if chart_indicator == 'sma':
+        pass
+    elif chart_indicator == 'ema':
+        pass
+
 
 def animate(i):
     """For animation function purposes. It reloads the data basing on the given time frame"""
-    data_handler = DataHandler()
-    history_data = data_handler.read_from_api(request_type='history', pair_choice=user_pair_choice,
-                                              candles_count=user_candles_count_choice,
-                                              set_granularity=user_granularity_choice, streaming_type="pricing")
-    a.clear()
-    # a.plot(history_data.iloc[139:]['time'], history_data.iloc[139:]['c'])
-    # a.locator_params(axis='both', nbins=2)
-    # a.plot([datetime.strptime(x, '%Y-%m-%d %H:%M').strftime('%m-%d %H:%M') for x in history_data['time'][:]],
-    #             [float(x) for x in history_data.iloc[:]['c']], label=major_pairs[user_pair_choice], ls='-')
-    history_data['time'] = np.array(history_data['time']).astype("datetime64[s]")
-    pricing_dates = history_data['time'].tolist()
-    a.plot(pricing_dates, [float(x) for x in history_data.iloc[:]['c']], color='#00A3E0',
-           label=major_pairs[user_pair_choice], ls='-')
-    a.set_title("Chart of %s \n Last price: %s" % (major_pairs[user_pair_choice], history_data['c'][149]))
+    global animation_status
+    if animation_status:
+        data_handler = DataHandler()
+        history_data = data_handler.read_from_api(request_type='history', pair_choice=user_pair_choice,
+                                                  candles_count=user_candles_count_choice+bottom_indicator[2]+
+                                                                chart_indicator[1],
+                                                  set_granularity=user_granularity_choice, streaming_type="pricing")
+        a.clear()
+        history_data['time'] = np.array(history_data['time']).astype("datetime64[s]")
+        pricing_dates = history_data['time'].tolist()
+        a.plot(pricing_dates, [float(x) for x in history_data.iloc[:user_candles_count_choice]['c']], color='#00A3E0',
+               label=major_pairs[user_pair_choice], ls='-')
+        a.set_title("Chart of %s \n Last price: %s" % (major_pairs[user_pair_choice], history_data['c'][149]))
 
 
 class Application(tk.Tk):
@@ -146,6 +286,19 @@ class Application(tk.Tk):
         pair_choice.add_command(label='USD_JPY', command=lambda: pair_choice_changer(8))
         menubar.add_cascade(label="Pair", menu=pair_choice)
 
+        indicators = tk.Menu(menubar, tearoff=1)
+        indicators.add_command(label="RSI", command=lambda: set_indicators(bottom='rsi'))
+        indicators.add_command(label="MACD", command=lambda: set_indicators(bottom='macd'))
+        indicators.add_separator()
+        indicators.add_command(label="SMA", command=lambda: set_indicators(chart='sma'))
+        indicators.add_command(label="EMA", command=lambda: set_indicators(chart='ema'))
+        menubar.add_cascade(label='Indicators', menu=indicators)
+
+        start_stop = tk.Menu(menubar, tearoff=1)
+        start_stop.add_command(label="Start", command=lambda: animation_changer(status='on'))
+        start_stop.add_command(label="Stop", command=lambda: animation_changer(status='off'))
+        menubar.add_cascade(label="Auto-update", menu=start_stop)
+
         tk.Tk.config(self, menu=menubar)
 
         self.frames = {}
@@ -166,12 +319,12 @@ class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label = tk.Label(self, text="Home Page", font=LARGE_FONT)
         label.pack(padx=10, pady=10)
         # Using lambda function to prevent from immediate initialization
-        button1 = ttk.Button(self, text="Page One", command=lambda: controller.show_frame(PageOne))
+        button1 = ttk.Button(self, text="Static chart", command=lambda: controller.show_frame(PageOne))
         button1.pack()
-        button2 = ttk.Button(self, text="Page Two", command=lambda: controller.show_frame(PageTwo))
+        button2 = ttk.Button(self, text="Dynamic chart", command=lambda: controller.show_frame(PageTwo))
         button2.pack()
 
 
@@ -179,7 +332,7 @@ class PageOne(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="First Page", font=LARGE_FONT)
+        label = tk.Label(self, text="Static chart page", font=LARGE_FONT)
         label.pack(padx=10, pady=10)
         # Using lambda function to prevent from immediate initialization
         button1 = ttk.Button(self, text="Return to Home Page", command=lambda: controller.show_frame(StartPage))
@@ -216,7 +369,7 @@ class PageTwo(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text='Page Two', font=LARGE_FONT)
+        label = tk.Label(self, text='Dynamic chart page', font=LARGE_FONT)
         label.pack(padx=10, pady=10)
         button1 = ttk.Button(self, text='Return to Home Page', command=lambda: controller.show_frame(StartPage))
         button1.pack()
--- Utils.py
+++
@@ -1,2 +0,0 @@
-connection_data = {'minimum_l': 1, 'maximum_l': 10, 'database_l': 'Quote', 'user_l': 'postgres',
-                   'password_l': 'As12g612#$', 'host_l': 'localhost'}
